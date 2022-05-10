363789fb449f86a9a66ca4e3fc74739390f76386
chiumanfelix <chiumanfelix@gmail.com>
2022-05-10 16:57:44 +0700

    dikit update

--- app/src/main/java/com/felix/datastore2/MainActivity.kt
+++ app/src/main/java/com/felix/datastore2/MainActivity.kt
@@ -2,8 +2,8 @@ package com.felix.datastore2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import com.felix.datastore2.databinding.ActivityMainBinding
 
@@ -27,10 +27,23 @@ class MainActivity : AppCompatActivity() {
     private fun setObserver() {
         viewModel.apply {
             getDataStore().observe(this@MainActivity){
-                binding.etPass.text.toString()
+                binding.etPass.setText(it)
+                binding.btnLogin.setOnClickListener {result ->
+                    Log.d("resultan", it.toString())
+                    Log.d("edittext", binding.etPass.text.toString())
+                    if (binding.etPass.text.toString().equals(result)){
+                        Toast.makeText(this@MainActivity, "login sukses", Toast.LENGTH_SHORT).show()
+                    }
+                    else if (binding.etPass.text.toString().equals("")){
+                        Toast.makeText(this@MainActivity, "kosong", Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toast.makeText(this@MainActivity, "login gagal", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
             vText.observe(this@MainActivity){result ->
-                binding.etPass.text.toString()
+                binding.etPass.setText(result)
             }
         }
     }
@@ -40,17 +53,7 @@ class MainActivity : AppCompatActivity() {
             btnSet.setOnClickListener {
                 viewModel.saveDataStore(binding.etPass.text.toString())
             }
-            btnLogin.setOnClickListener {
-                if (binding.etPass.text.toString().equals(viewModel.vText)){
-                    Toast.makeText(this@MainActivity, "login sukses", Toast.LENGTH_SHORT).show()
-                }
-                else if (binding.etPass.text.toString().equals("")){
-                    Toast.makeText(this@MainActivity, "kosong", Toast.LENGTH_SHORT).show()
-                }
-                else{
-                    Toast.makeText(this@MainActivity, "login gagal", Toast.LENGTH_SHORT).show()
-                }
-            }
+
         }
     }
 }
